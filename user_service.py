dc5098a83117abfcd4e9322672d60def8e62a083
seojeong0120 <elley0120@yonsei.ac.kr>
2025-07-16 10:06:40 +0900

    feat: login, update_user_pwd implement

--- user_service.py
+++ user_service.py
@@ -6,29 +6,21 @@ class UserService:
         self.repo = userRepoitory
 
     def login(self, user_login: UserLogin) -> User:
-        ## TODO #로그인 기능 구현 
-        user = None
+        # 이메일로 사용자를 조회하기
+        user = self.repo.get_user_by_email(user_login.email)
+        # 사용자가 존재하지 않으면 오류 발생
+        if not user:
+            raise ValueError("User not Found.")
+        # 비밀번호가 일치하지 않으면 오류 발생
+        if user.password != user_login.password:
+            raise ValueError("Invalid ID/PW")
+        # 이메일과 비밀번호가 모두 일치하면 사용자 정보 반환
         return user
         
     def register_user(self, new_user: User) -> User:
-        """
-        새로운 사용자 등록 
-        Parameters: 
-        new_user: 등록할 사용자 정보 (email, password, username)
-        
-        Returns: 
-        User: 등록된 사용자 정보 
-
-        Raises: 
-        ValueError: 이미 해당 이메일의 사용자가 존재하는 경우 
-        """
-        existing_user = self.repo.get_user_by_email(new_user.email)
-        if existing_user: 
-            raise ValueError("User already Exists")
-        
-        self.repo.save_user(new_user)
+        ## TODO
+        new_user = None
         return new_user
-    
 
     def delete_user(self, email: str) -> User:
         ## TODO        
@@ -36,7 +28,17 @@ class UserService:
         return deleted_user
 
     def update_user_pwd(self, user_update: UserUpdate) -> User:
-        ## TODO
+        # 반환할 사용자 객체를 초기화
         updated_user = None
+        # 이메일로 사용자를 조회하기
+        user = self.repo.get_user_by_email(user_update.email)
+        # 사용자가 존재하지 않으면 오류 발생
+        if user is None:
+            raise ValueError("User not found.")
+        # 비밀번호를 새 비밀번호로 업데이트
+        user.password = user_update.new_password
+        # 변경된 정보를 저장하고 저장된 사용자 객체 반환
+        updated_user = self.repo.update_user(user)
+        # 업데이트된 사용자 객체 반환
         return updated_user
         
