50225b8a60103311e5ba69881c504219e1f435e1
seojeong0120 <elley0120@yonsei.ac.kr>
2025-07-21 23:00:42 +0900

    fix preprocess function

--- YBIGTA_newbie_team_project/review_analysis/preprocessing/general_review_processor.py
+++ YBIGTA_newbie_team_project/review_analysis/preprocessing/general_review_processor.py
@@ -13,7 +13,7 @@ class GeneralReviewProcessor(BaseDataProcessor):
 
     def preprocess(self):
         # 결측치 제거
-        self.df.dropna(subset=["rating", "review", "date"], inplace=True)
+        self.df.dropna(subset=["rating", "date", "content"], inplace=True)
 
         # 별점 범위 이상치 제거
         self.df = self.df[self.df["rating"].between(1, 5)]
@@ -22,18 +22,18 @@ class GeneralReviewProcessor(BaseDataProcessor):
         today = datetime.today()
         self.df = self.df[(self.df["date"] >= pd.Timestamp("2022-01-01")) & (self.df["date"] <= today)]
 
-        # 특수문제 제거
-        self.df["review"] = self.df["review"].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+        # 특수문자 제거
+        self.df["content"] = self.df["content"].apply(lambda x: re.sub(r'[^\w\s]', '', x))
 
         # 리뷰 길이 제한
-        self.df["review_length"] = self.df["review"].apply(len)
+        self.df["review_length"] = self.df["content"].apply(len)
         self.df = self.df[(self.df["review_length"] > 5) & (self.df["review_length"] < 1000)]
 
         # 공백 문자열 제거
-        self.df["review"] = self.df["review"].str.strip()
+        self.df["content"] = self.df["content"].str.strip()
 
         # 중복 리뷰 제거
-        self.df.drop_duplicates(subset=["review"], inplace=True)
+        self.df.drop_duplicates(subset=["content"], inplace=True)
         
         # 리뷰 텍스트 임베딩
         model = SentenceTransformer("all-MiniLM-L6-v2")
