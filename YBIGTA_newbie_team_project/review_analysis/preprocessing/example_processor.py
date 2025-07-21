4aa2327be68e891fb2940523158442878adc6561
cypark00 <your_email@example.com>
2025-07-21 21:51:28 +0900

    feat : add weekday and text_length features for feature_engineering

--- YBIGTA_newbie_team_project/review_analysis/preprocessing/example_processor.py
+++ YBIGTA_newbie_team_project/review_analysis/preprocessing/example_processor.py
@@ -8,7 +8,16 @@ class ExampleProcessor(BaseDataProcessor):
         pass
     
     def feature_engineering(self):
-        pass
+        #날짜 -> 요일 파생 변수 생성 
+        import pandas as pd
+        self.df["date"] = pd.to_datetime(self.df["date"], errors="coerce")
+        self.df["weekday"] = self.df["date"].dt.day_name()
+
+        #텍스트 길이 파생 변수 생성 
+        self.df["text_length"] = self.df["content"].astype(str).apply(len)
+
+
+
 
     def save_to_database(self):
         pass
